b450f59b7007496d26a4c949596821da7b61f28d
Saba <CodeSaba0@gmail.com>
2019-09-09 03:18:30 -0400

    Saba's initial submission

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-const { mongoose } = require('./db.js');
+const { mongoose } = require("./db.js");
 
 const port = process.env.PORT_NJS_INVENTORY || 3000;
 const portAngular = process.env.PORT_NG_INVENTORY || 4200;
@@ -12,8 +12,8 @@ var app = express();
 app.use(bodyParser.json());
 // TODO_FA do we need to do this
 //app.use(bodyParser.urlencoded({ extended: true }))
+//Saba is here making her first change
 
-app.use(cors({origin: `http://localhost:${portAngular}`}));
+app.use(cors({ origin: `http://localhost:${portAngular}` }));
 
 app.listen(port, () => console.log(`Server started at port : ${port}`));
-
