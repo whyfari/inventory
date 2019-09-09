37a91d0b5ca3d25e44adcc1eac85f03ca5c5135f
whyfari <whyfari@gmail.com>
2019-09-09 02:57:49 -0400

    initial submission

---
+++ nodejs/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const { mongoose } = require('./db.js');
+
+const port = process.env.PORT_NJS_INVENTORY || 3000;
+const portAngular = process.env.PORT_NG_INVENTORY || 4200;
+
+var app = express();
+
+app.use(bodyParser.json());
+// TODO_FA do we need to do this
+//app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(cors({origin: `http://localhost:${portAngular}`}));
+
+app.listen(port, () => console.log(`Server started at port : ${port}`));
+
